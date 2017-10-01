5a0e43d708370ed36bc91b4635c434bda83c4da9
Mishchenko, Gleb <gleb.mishchenko@emc.com>
2017-10-01 12:01:34 +0300

    Refactored numpy array usages in more numpy way

--- water-surface/Surface.py
+++ water-surface/Surface.py
@@ -13,23 +13,28 @@ class Surface(object):
         for each in range(nwave):
             self.waves.append(Wave.random_wave(nwave))
 
-        shape = (self.size[0], self.size[1], 2)
-        self.grid = np.zeros(shape, dtype=np.float32)
-
     def position(self):
-        x = np.linspace(-1, 1, self.size[0])
-        y = np.linspace(-1, 1, self.size[1])
+        x, y = self.coords()
 
-        for i in range(self.size[0]):
-            for j in range(self.size[1]):
-                self.grid[i, j] = (x[i], y[j])
+        shape = (self.size[0], self.size[1], 2)
+        grid = self.empty_arr(shape)
 
-        return self.grid
+        grid[..., 0] = x
+        grid[..., 1] = y
+        return grid
 
-    def height(self, time=0):
-        height = np.zeros(self.size, dtype=np.float32)
+    def coords(self):
         x = np.linspace(-1, 1, self.size[0])[:, None]
         y = np.linspace(-1, 1, self.size[1])[None, :]
+
+        return x, y
+
+    def empty_arr(self, shape):
+        return np.zeros(shape, dtype=np.float32)
+
+    def height(self, time=0):
+        x, y = self.coords()
+        height = self.empty_arr(self.size)
 
         for wave in self.waves:
             height[:, :] += wave.amplitude * \
