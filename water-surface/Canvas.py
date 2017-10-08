163d9682d2c385dd57c1da670db8bd40fc483549
utkillr <nosikov-k@mail.ru>
2017-10-08 15:05:07 +0400

    Lab5 done

--- water-surface/Canvas.py
+++ water-surface/Canvas.py
@@ -24,12 +24,16 @@ class Canvas(app.Canvas):
         self.surface = Surface()
         self.triangles = gloo.IndexBuffer(self.surface.triangulation())
         self.sun = Sun()
+        self.are_points_visible = False
 
-        self.program = gloo.Program(shaders.vert_shader, shaders.frag_shader)
+        self.program = gloo.Program(shaders.vert_shader, shaders.frag_shader_triangle)
         self.program['a_position'] = self.surface.position()
-        self.program['u_sun_direction'] = self.sun.direction()
         self.program['u_sun_color'] = self.sun.color
         self.program['u_ambient_color'] = Surface.ambient_color()
+        self.program['u_sun_direction'] = self.sun.direction(self.time)
+
+        self.program_point = gloo.Program(shaders.vert_shader, shaders.frag_shader_point)
+        self.program_point["a_position"] = self.surface.position()
 
         self.timer = app.Timer('auto', connect=self.on_timer, start=True)
         self.activate_zoom()
@@ -40,13 +44,25 @@ class Canvas(app.Canvas):
         gloo.set_viewport(0, 0, *self.physical_size)
 
     def on_draw(self, event):
+        gloo.set_state(clear_color=(0, 0, 0, 1), blend=False)
         gloo.clear()
-        self.program['a_height'] = self.surface.height(self.time)
+        height = self.surface.height(self.time)
+        self.program['a_height'] = height
         self.program['a_normal'] = self.surface.normal(self.time)
+
+        # draw triangles
+        gloo.set_state(depth_test=True)
         self.program.draw('triangles', self.triangles)
+
+        # get glares
+        if self.are_points_visible:
+            self.program_point['a_height'] = height
+            gloo.set_state(depth_test=False)
+            self.program_point.draw('points')
 
     def on_timer(self, event):
         self.time += 0.01
+        self.program['u_sun_direction'] = self.sun.direction(self.time)
         # calls on_draw
         self.update()
 
