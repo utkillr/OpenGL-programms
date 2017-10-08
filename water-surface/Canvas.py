1810a05850b45140cbe68ef70f4b31320c15d3d7
utkillr <nosikov-k@mail.ru>
2017-10-08 15:41:51 +0400

    Completed lab1_task6

--- water-surface/Canvas.py
+++ water-surface/Canvas.py
@@ -1,15 +1,15 @@
 
 from vispy import app
 from vispy import gloo
+from vispy import io
 
 import shaders
-from Surface import Surface
 from Sun import Sun
 
 
 class Canvas(app.Canvas):
 
-    def __init__(self, size=(600, 600)):
+    def __init__(self, surface, sky="clouds.png", size=(600, 600)):
         # app window dimensions
         self.width = size[0]
         self.height = size[1]
@@ -17,23 +17,24 @@ class Canvas(app.Canvas):
         # initial time to count heights of points
         self.time = 0
 
-        app.Canvas.__init__(self, size=(self.width, self.height), title='Water surface simulator')
+        app.Canvas.__init__(self, size=(self.width, self.height), title='Water surface with clouds simulator')
 
         gloo.set_state(clear_color=(0, 0, 0, 1), depth_test=True, blend=False)
 
-        self.surface = Surface()
+        self.surface = surface
+        self.sky = io.read_png(sky)
         self.triangles = gloo.IndexBuffer(self.surface.triangulation())
         self.sun = Sun()
         self.are_points_visible = False
 
+        position = self.surface.position()
+
         self.program = gloo.Program(shaders.vert_shader, shaders.frag_shader_triangle)
-        self.program['a_position'] = self.surface.position()
-        self.program['u_sun_color'] = self.sun.color
-        self.program['u_ambient_color'] = Surface.ambient_color()
-        self.program['u_sun_direction'] = self.sun.direction(self.time)
+        self.program['a_position'] = position
+        self.program['u_sky_texture'] = gloo.Texture2D(self.sky, wrapping='repeat', interpolation='linear')
 
         self.program_point = gloo.Program(shaders.vert_shader, shaders.frag_shader_point)
-        self.program_point["a_position"] = self.surface.position()
+        self.program_point["a_position"] = position
 
         self.timer = app.Timer('auto', connect=self.on_timer, start=True)
         self.activate_zoom()
@@ -47,8 +48,9 @@ class Canvas(app.Canvas):
         gloo.set_state(clear_color=(0, 0, 0, 1), blend=False)
         gloo.clear()
         height = self.surface.height(self.time)
+        normal = self.surface.normal(self.time)
         self.program['a_height'] = height
-        self.program['a_normal'] = self.surface.normal(self.time)
+        self.program['a_normal'] = normal
 
         # draw triangles
         gloo.set_state(depth_test=True)
@@ -68,3 +70,10 @@ class Canvas(app.Canvas):
 
     def on_resize(self, event):
         self.activate_zoom()
+
+    def on_key_press(self, event):
+        if event.key == 'Escape':
+            self.close()
+        elif event.key == ' ':
+            self.are_points_visible = not self.are_points_visible
+
--- water-surface/main.py
+++ water-surface/main.py
@@ -1,7 +1,8 @@
 
 from Canvas import Canvas
 from vispy import app
+from Surface import Surface
 
 if __name__ == '__main__':
-    c = Canvas()
+    c = Canvas(Surface())
     app.run()
